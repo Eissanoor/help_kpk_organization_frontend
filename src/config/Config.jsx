338dfa5acd8983a90379a07da52d9bd4e713320b
Eissanoor <eissaanoor@gmail.com>
2024-12-30 14:36:18 +0500

    Update API base URL to a secure production endpoint. Changed API_BASE_URL in Config.jsx from 'http://fundapi.pakardi.com' to 'https://help-kpk-organization-backend-sigma.vercel.app' for improved security and reliability.

--- src/config/Config.jsx
+++ src/config/Config.jsx
@@ -1,3 +1,4 @@
 // src/config/config.jsx
 // export const API_BASE_URL = 'http://localhost:5020';
-export const API_BASE_URL = 'http://fundapi.pakardi.com';
+// export const API_BASE_URL = 'http://fundapi.pakardi.com';
+export const API_BASE_URL = 'https://help-kpk-organization-backend-sigma.vercel.app';
